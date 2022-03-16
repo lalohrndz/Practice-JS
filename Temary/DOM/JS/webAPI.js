80bae2ee7890f70b73913e771fb325a3445460bf
Eduardo Pastor <edo.pstr@gmail.com>
2022-03-15 19:28:27 -0600

    web api

--- Temary/DOM/JS/webAPI.js
+++ Temary/DOM/JS/webAPI.js
@@ -6,9 +6,21 @@
 ? tratarse de un elemento, una cadena de texto o un comentario).
 */
 
+/* 
+.- Elementos del documento [ https://developer.mozilla.org/es/docs/Web/HTML/Element ]
+*/
 console.log("window - ",window);
-console.log("document - ",Document);
-
+console.log("document - ",document);
+console.log("window.document - ",document.head);
+console.log("window.document - ",document.body);
+console.log("window.document - ",document.charset);
+console.log("window.document - ",document.title);
+console.log("window.document - ",document.links);
+console.log("window.document - ",document.forms);
+console.log("window.document - ",document.scripts);
+setTimeout(() => {
+    console.log("window.document - ",document.getSelection().toString());
+}, 3000);
 
 const talk = ()=>{
     let text = document.getElementById("text-talk").value;
