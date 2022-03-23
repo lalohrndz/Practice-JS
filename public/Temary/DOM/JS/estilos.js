cfd991106740e02a3d93655d16be3af975a17157
Eduardo Pastor <edo.pstr@gmail.com>
2022-03-23 00:11:30 -0600

    navbar mobile integration, html templates, media queries

--- public/Temary/DOM/JS/estilos.js
+++ public/Temary/DOM/JS/estilos.js
@@ -2,9 +2,10 @@ const $linkDOM = document.querySelector(".link-DOM");
 
 $linkDOM.style.backgroundColor = "#fafafa"
 $linkDOM.style.color = "#aaa"
-
+/* 
 console.log($linkDOM.style);
 console.log($linkDOM.getAttribute("style"));
 console.log($linkDOM.style.backgroundColor);
 console.log($linkDOM.style.color);
 console.log(window.getComputedStyle($linkDOM));
+ */
