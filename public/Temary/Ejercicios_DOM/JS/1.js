f580a512cf74455984aa13d9807a0568a88c0b3b
Eduardo Pastor <edo.pstr@gmail.com>
2022-05-05 01:35:52 -0500

    1 exe ready

---
+++ public/Temary/Ejercicios_DOM/JS/1.js
@@ -0,0 +1,17 @@
+$btn = document.getElementById("ejMenu");
+$icon = document.getElementById("btnIcon");
+$slideMenu = document.getElementById("slideMenu");
+
+$btn.addEventListener("click",e=>{
+    if($icon.innerHTML == "add"){
+        $icon.innerHTML = "remove";
+        $slideMenu.style.display = "flex"
+    }else{
+        $icon.innerHTML = "add";
+        $slideMenu.style.display = "none"
+    }
+})
+
+$slideMenu.addEventListener("click",e=>{
+    e.target.style.display = "none"
+})
